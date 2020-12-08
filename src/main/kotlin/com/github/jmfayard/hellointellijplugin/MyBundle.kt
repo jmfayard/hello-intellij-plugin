037ddb5b3d8de311c6c74a64e186c9b45e7be902
GitHub Action <action@github.com>
2020-12-08 14:01:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/jmfayard/hellointellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jmfayard/hellointellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jmfayard.hellointellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jmfayard/hellointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jmfayard.hellointellijplugin.services
+
+import com.github.jmfayard.hellointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
